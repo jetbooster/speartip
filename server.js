e422ee88f1b42137f9dd8df798500858bfedba35
Sam Jarvis <jarvisam@gmail.com>
2018-05-28 17:15:15 +0000

    migrated static file transfer to package.json

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
+#!/usr/bin/env node
 const express = require("express");
 const path = require("path");
 const https = require('https');
 const fs = require('fs');
 const proxy = require("express-http-proxy");
 const config = require("./server.config.js");
-
-// screw figuring out webpack to move two files
-fs.copyFileSync('./src/resources/manifest.json','./dist/assets/manifest.json')
-fs.copyFileSync('./src/resources/speartip.ico','./dist/assets/media/speartip.ico')
 
 const app = express();
 
@@ -19,7 +16,7 @@ const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/speartipsolutions.co.uk/privkey.pem'),
 }
 
-express.createServer().get('*', (req,res)=>{
+express().get('*', (req,res)=>{
   res.redirect(`https://${req.headers.host}${req.url}`)
 }).listen(80);
 https.createServer(options,app).listen(443)
