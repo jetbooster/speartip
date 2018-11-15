bd11e6b7e26f0fca2883454e132de640afc4123b
Sam Jarvis <jarvisam@gmail.com>
2018-11-15 20:31:39 +0000

    Add further config

--- server.config.js
+++ server.config.js
@@ -1,4 +1,12 @@
+if (!(process.env.FULL_CHAIN_LOCATION && process.env.CERT_KEY_LOCATION)) {
+  throw Error('FULL_CHAIN_LOCATION and CERT_KEY_LOCATION must be set.');
+}
+
 module.exports = {
+  https: {
+    cert: process.env.FULL_CHAIN_LOCATION,
+    key: process.env.CERT_KEY_LOCATION,
+  },
   api: {
     hostname: 'localhost',
     port: 4001,
